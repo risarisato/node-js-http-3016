2f784de9bfef463811c2d01fe647e54d4f499335
risarisato <nqg44226@gmail.com>
2023-01-07 13:58:14 +0900

    ログから日付を除去

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ const http = require('http');
 const pug = require('pug');
 const server = http
   .createServer((req, res) => {
-    const now = new Date();
-    console.info(`[${now}] Requested by ${req.socket.remoteAddress}`);
+    console.info(`Requested by ${req.socket.remoteAddress}`);
     res.writeHead(200, {
       'Content-Type': 'text/html; charset=utf-8'
     });
@@ -13,7 +12,7 @@ const server = http
       case 'GET':
         if (req.url === '/') {
           res.write('<!DOCTYPE html><html lang="ja"><body>' +
-            '<h1>アンケートフォーム</h1>' +
+            '<h1>アンケートフォーム20230107</h1>' +
             '<a href="/enquetes">アンケート一覧</a>' +
             '</body></html>');
         } else if (req.url === '/enquetes') {
@@ -57,7 +56,7 @@ const server = http
           .on('end', () => {
             const answer = new URLSearchParams(rawData);
             const body = `${answer.get('name')}さんは${answer.get('favorite')}に投票しました`;
-            console.info(`[${now}] ${body}`);
+            console.info(body);
             res.write(`<!DOCTYPE html><html lang="ja"><body><h1>${body}</h1></body></html>`);
             res.end();
           });
@@ -67,12 +66,12 @@ const server = http
     }
   })
   .on('error', e => {
-    console.error(`[${new Date()}] Server Error`, e);
+    console.error('Server Error', e);
   })
   .on('clientError', e => {
-    console.error(`[${new Date()}] Client Error`, e);
+    console.error('Client Error', e);
   });
-  const port = process.env.PORT || 8000;
+const port = process.env.PORT || 8000;
 server.listen(port, () => {
-  console.info(`[${new Date()}] Listening on ${port}`);
+  console.info(`Listening on ${port}`);
 });
